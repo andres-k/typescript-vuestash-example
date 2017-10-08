5ec3db8987481671a00f82a29fc3723219772cf8
Andres Keskküla <andres.kesk@gmail.com>
2017-10-08 16:13:47 +0300

    Typescript implemented

---
+++ src/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p> {{ msg }} </p>
+        <incrementComponent/>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import IncrementComponent from './Increment.vue';
+
+@Component({
+    components:{ 
+        "incrementComponent": IncrementComponent}
+})
+export default class App extends Vue {
+    msg: string = "Message";
+}
+</script>
---
+++ src/components/Increment.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>
+            {{ incrementalValue }}
+        </p>
+        <button @click="increaseValue">Add!</button>
+        <button @click="decreaseValue">Decrease!</button>
+        
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class IncrementComponent extends Vue {
+    incrementalValue: number = 0;
+
+    increaseValue() {
+        this.incrementalValue++;
+    }
+
+    decreaseValue() {
+        this.incrementalValue--;
+    }
+}
+</script>
